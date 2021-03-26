565c0a6f74343f06808ab7aed045e47ffaea8141
Enhancer <xjhxyshz@126.com>
2021-03-26 14:10:36 +0800

    format code

--- Editor/Analyzer/Impl/MainThreadAnalyzeToFile.cs
+++ Editor/Analyzer/Impl/MainThreadAnalyzeToFile.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-
 using UTJ.ProfilerReader.BinaryData;
 
 
@@ -9,49 +8,45 @@ namespace UTJ.ProfilerReader.Analyzer
     {
         private class SampleData
         {
-            public string fullName;
-            public string sampleName;
+            public readonly string fullName;
+            public readonly string sampleName;
+            public readonly string categoryName;
 
-            public float totalSelfMsec = 0.0f;
-            public float selfMinMSec = float.MaxValue;
-            public float selfMaxMsec = 0.0f;
+            public float TotalSelfMsec = 0.0f;
+            public float SelfMinMSec = float.MaxValue;
+            public float SelfMaxMsec = 0.0f;
 
-            public float totalExecMsec = 0.0f;
-            public float execMinMSec = float.MaxValue;
-            public float execMaxMsec = 0.0f;
+            public float TotalExecMsec = 0.0f;
+            public float ExecMinMSec = float.MaxValue;
+            public float ExecMaxMsec = 0.0f;
 
-            public int callNum = 0;
+            public int CallNum = 0;
 
-            public string categoryName;
-
-            public SampleData(string fname, string name,string category)
+            public SampleData(string fullName, string sampleName, string categoryName)
             {
-                this.sampleName = name;
-                this.fullName = fname;
-                this.categoryName = category;
+                this.sampleName = sampleName;
+                this.fullName = fullName;
+                this.categoryName = categoryName;
             }
 
             public void Called(float selfMsec, float execMsec)
             {
+                SelfMinMSec = ProfilerLogUtil.Min(SelfMinMSec, selfMsec);
+                SelfMaxMsec = ProfilerLogUtil.Max(SelfMaxMsec, selfMsec);
+                TotalSelfMsec += selfMsec;
 
-                selfMinMSec = ProfilerLogUtil.Min(selfMinMSec, selfMsec);
-                selfMaxMsec = ProfilerLogUtil.Max(selfMaxMsec, selfMsec);
-                totalSelfMsec += selfMsec;
-
-                execMinMSec = ProfilerLogUtil.Min(execMinMSec, execMsec);
-                execMaxMsec = ProfilerLogUtil.Max(execMaxMsec, execMsec);
-                totalExecMsec += execMsec;
-                ++callNum;
+                ExecMinMSec = ProfilerLogUtil.Min(ExecMinMSec, execMsec);
+                ExecMaxMsec = ProfilerLogUtil.Max(ExecMaxMsec, execMsec);
+                TotalExecMsec += execMsec;
+                ++CallNum;
             }
         }
-        private Dictionary<string, SampleData> samples = new Dictionary<string, SampleData>();
-        private int frameNum = 0;
 
+        private readonly Dictionary<string, SampleData> _samples = new Dictionary<string, SampleData>();
+        private int _frameNum = 0;
 
         public override void CollectData(ProfilerFrameData frameData)
         {
-            // 特別枠で frameDataのＣＰＵ時間を追加
-            // 同一フレーム内に同じスレッド名が複数できるので…
             Dictionary<string, int> threadNameCounter = new Dictionary<string, int>(8);
             foreach (var thread in frameData.m_ThreadData)
             {
@@ -60,12 +55,17 @@ namespace UTJ.ProfilerReader.Analyzer
                     CollectThread(thread);
                 }
             }
-            ++frameNum;
+
+            ++_frameNum;
         }
 
         private void CollectThread(ThreadData thread)
         {
-            if (thread == null || thread.m_AllSamples == null) { return; }
+            if (thread?.m_AllSamples == null)
+            {
+                return;
+            }
+
             foreach (var sample in thread.m_AllSamples)
             {
                 if (sample.parent == null)
@@ -79,9 +79,11 @@ namespace UTJ.ProfilerReader.Analyzer
         {
             if (!string.IsNullOrEmpty(sample.sampleName))
             {
-                string category = UTJ.ProfilerReader.ProtocolData.GetCategory(unityVersion, sample.group);
-                AddSampleData(sample.fullSampleName, sample.sampleName, category, sample.selfTimeUs / 1000.0f, sample.timeUS / 1000.0f);
+                string category = ProtocolData.GetCategory(unityVersion, sample.group);
+                AddSampleData(sample.fullSampleName, sample.sampleName, category, sample.selfTimeUs / 1000.0f,
+                    sample.timeUS / 1000.0f);
             }
+
             if (sample.children != null)
             {
                 foreach (var child in sample.children)
@@ -89,83 +91,76 @@ namespace UTJ.ProfilerReader.Analyzer
                     CollectFromNamedChildren(child);
                 }
             }
-            return;
         }
 
-        private void AddSampleData(string fullName, string sampleName,string categoryName, float selfMsec, float execMsec)
+        private void AddSampleData(string fullName, string sampleName, string categoryName, float selfMsec,
+            float execMsec)
         {
-
-            SampleData sampleData = null;
             if (selfMsec < 0.0f)
             {
                 ProfilerLogUtil.logErrorString("minus Param " + sampleName + ":" + selfMsec + ":" + execMsec);
                 return;
             }
+
             if (selfMsec > 1000.0f * 50.0f)
             {
                 ProfilerLogUtil.logErrorString("minus Param " + sampleName + ":" + selfMsec + ":" + execMsec);
                 return;
             }
 
-            if (!this.samples.TryGetValue(fullName, out sampleData))
+            if (!_samples.TryGetValue(fullName, out var sampleData))
             {
-                sampleData = new SampleData(fullName, sampleName,categoryName);
-                this.samples.Add(fullName, sampleData);
+                sampleData = new SampleData(fullName, sampleName, categoryName);
+                _samples.Add(fullName, sampleData);
             }
+
             sampleData.Called(selfMsec, execMsec);
         }
 
-        /// <summary>
-        /// 結果書き出し
-        /// </summary>
         protected override string GetResultText()
         {
             CsvStringGenerator csvStringGenerator = new CsvStringGenerator();
-            csvStringGenerator.AppendColumn("name").AppendColumn("fullname").AppendColumn("category").AppendColumn("callNum").
-                AppendColumn("self").AppendColumn("sum(msec)").AppendColumn("perFrame(msec)").AppendColumn("min(msec)").AppendColumn("max(msec)").
-                AppendColumn("total").AppendColumn("sum(msec)").AppendColumn("perFrame(msec)").AppendColumn("min(msec)").AppendColumn("max(msec)").
-                NextRow();
-            var sampleDataList = new List<SampleData>(samples.Values);
+            csvStringGenerator.AppendColumn("name").AppendColumn("fullname").AppendColumn("category")
+                .AppendColumn("callNum").AppendColumn("self").AppendColumn("sum(msec)").AppendColumn("perFrame(msec)")
+                .AppendColumn("min(msec)").AppendColumn("max(msec)").AppendColumn("total").AppendColumn("sum(msec)")
+                .AppendColumn("perFrame(msec)").AppendColumn("min(msec)").AppendColumn("max(msec)").NextRow();
+            var sampleDataList = new List<SampleData>(_samples.Values);
             sampleDataList.Sort((a, b) =>
             {
-                if (a.totalSelfMsec > b.totalSelfMsec)
+                if (a.TotalSelfMsec > b.TotalSelfMsec)
                 {
                     return -1;
-                }else if (a.totalSelfMsec < b.totalSelfMsec)
+                }
+
+                if (a.TotalSelfMsec < b.TotalSelfMsec)
                 {
                     return 1;
                 }
+
                 return 0;
             });
             foreach (var sampleData in sampleDataList)
             {
-                csvStringGenerator.AppendColumn(sampleData.sampleName).
-                    AppendColumn(sampleData.fullName).
-                    AppendColumn(sampleData.categoryName).
-                    AppendColumn(sampleData.callNum).AppendColumn("");
+                csvStringGenerator.AppendColumn(sampleData.sampleName).AppendColumn(sampleData.fullName)
+                    .AppendColumn(sampleData.categoryName).AppendColumn(sampleData.CallNum).AppendColumn("");
 
-                csvStringGenerator.AppendColumn(sampleData.totalSelfMsec).
-                    AppendColumn(sampleData.totalSelfMsec / frameNum).
-                    AppendColumn(sampleData.selfMinMSec).
-                    AppendColumn(sampleData.selfMaxMsec).AppendColumn("");
+                csvStringGenerator.AppendColumn(sampleData.TotalSelfMsec)
+                    .AppendColumn(sampleData.TotalSelfMsec / _frameNum).AppendColumn(sampleData.SelfMinMSec)
+                    .AppendColumn(sampleData.SelfMaxMsec).AppendColumn("");
 
 
-                csvStringGenerator.AppendColumn(sampleData.totalExecMsec).
-                    AppendColumn(sampleData.totalExecMsec / frameNum).
-                    AppendColumn(sampleData.execMinMSec).
-                    AppendColumn(sampleData.execMaxMsec);
+                csvStringGenerator.AppendColumn(sampleData.TotalExecMsec)
+                    .AppendColumn(sampleData.TotalExecMsec / _frameNum).AppendColumn(sampleData.ExecMinMSec)
+                    .AppendColumn(sampleData.ExecMaxMsec);
                 csvStringGenerator.NextRow();
             }
 
             return csvStringGenerator.ToString();
         }
+
         protected override string FooterName
         {
-            get
-            {
-                return "_main_self.csv";
-            }
+            get { return "_main_self.csv"; }
         }
-
     }
 }
