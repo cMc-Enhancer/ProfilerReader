430f736ee118a222e4bb006775f35248fa1f2083
Enhancer <xjhxyshz@126.com>
2021-03-27 15:43:10 +0800

    Implement AiScriptAnalyzeToFile

--- Editor/Analyzer/Impl/AiScriptAnalyzeToFile.cs
+++ Editor/Analyzer/Impl/AiScriptAnalyzeToFile.cs
@@ -1,157 +1,91 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UTJ.ProfilerReader.BinaryData;
-
 
 namespace UTJ.ProfilerReader.Analyzer
 {
     public class AiScriptAnalyzeToFile : AnalyzeToTextbaseFileBase
     {
-        private class SampleData
+        private class FrameSampleData
         {
-            public readonly string fullName;
-            public readonly string sampleName;
-            public readonly string categoryName;
-
-            public float TotalSelfMsec = 0.0f;
-            public float SelfMinMSec = float.MaxValue;
-            public float SelfMaxMsec = 0.0f;
-
-            public float TotalExecMsec = 0.0f;
-            public float ExecMinMSec = float.MaxValue;
-            public float ExecMaxMsec = 0.0f;
-
-            public int CallNum = 0;
-
-            public SampleData(string fullName, string sampleName, string categoryName)
-            {
-                this.sampleName = sampleName;
-                this.fullName = fullName;
-                this.categoryName = categoryName;
-            }
-
-            public void Called(float selfMsec, float execMsec)
-            {
-                SelfMinMSec = ProfilerLogUtil.Min(SelfMinMSec, selfMsec);
-                SelfMaxMsec = ProfilerLogUtil.Max(SelfMaxMsec, selfMsec);
-                TotalSelfMsec += selfMsec;
-
-                ExecMinMSec = ProfilerLogUtil.Min(ExecMinMSec, execMsec);
-                ExecMaxMsec = ProfilerLogUtil.Max(ExecMaxMsec, execMsec);
-                TotalExecMsec += execMsec;
-                ++CallNum;
-            }
+            public Dictionary<string, float> Samples { get; } = new Dictionary<string, float>();
         }
 
-        private readonly Dictionary<string, SampleData> _samples = new Dictionary<string, SampleData>();
-        private int _frameNum = 0;
+        private readonly List<FrameSampleData> frameSampleData = new List<FrameSampleData>();
+        private readonly HashSet<string> distinctSampleNames = new HashSet<string>();
 
         public override void CollectData(ProfilerFrameData frameData)
         {
-            Dictionary<string, int> threadNameCounter = new Dictionary<string, int>(8);
-            foreach (var thread in frameData.m_ThreadData)
+            FrameSampleData data = new FrameSampleData();
+
+            var samples = from thread in frameData.m_ThreadData
+                where thread.IsMainThread && thread.m_AllSamples != null
+                from sample in thread.m_AllSamples
+                where sample.parent == null
+                select sample;
+            foreach (var sample in samples)
             {
-                if (thread.IsMainThread)
-                {
-                    CollectThread(thread);
-                }
+                CollectSample(sample, data);
             }
 
-            ++_frameNum;
+            frameSampleData.Add(data);
         }
 
-        private void CollectThread(ThreadData thread)
+        private void CollectSample(ProfilerSample sample, FrameSampleData data)
         {
-            if (thread?.m_AllSamples == null)
+            string sampleName = sample.sampleName;
+            var isAiScript = !string.IsNullOrEmpty(sampleName) && IsAiScript(sampleName);
+            if (isAiScript)
             {
-                return;
-            }
-
-            foreach (var sample in thread.m_AllSamples)
-            {
-                if (sample.parent == null)
-                {
-                    CollectFromNamedChildren(sample);
-                }
-            }
-        }
-
-        private void CollectFromNamedChildren(ProfilerSample sample)
-        {
-            if (!string.IsNullOrEmpty(sample.sampleName))
-            {
-                string category = ProtocolData.GetCategory(unityVersion, sample.group);
-                AddSampleData(sample.fullSampleName, sample.sampleName, category, sample.selfTimeUs / 1000.0f,
-                    sample.timeUS / 1000.0f);
+                float execMsec = sample.timeUS / 1000.0f;
+                data.Samples.TryGetValue(sampleName, out float defaultValue);
+                data.Samples[sampleName] = defaultValue + execMsec;
             }
 
             if (sample.children != null)
             {
                 foreach (var child in sample.children)
                 {
-                    CollectFromNamedChildren(child);
+                    CollectSample(child, data);
                 }
             }
         }
 
-        private void AddSampleData(string fullName, string sampleName, string categoryName, float selfMsec,
-            float execMsec)
+        private bool IsAiScript(string name)
         {
-            if (selfMsec < 0.0f)
+            if (name.Contains("AiPlayer") || name.Contains("NeoPath"))
             {
-                ProfilerLogUtil.logErrorString("minus Param " + sampleName + ":" + selfMsec + ":" + execMsec);
-                return;
+                distinctSampleNames.Add(name);
+                return true;
             }
 
-            if (selfMsec > 1000.0f * 50.0f)
-            {
-                ProfilerLogUtil.logErrorString("minus Param " + sampleName + ":" + selfMsec + ":" + execMsec);
-                return;
-            }
-
-            if (!_samples.TryGetValue(fullName, out var sampleData))
-            {
-                sampleData = new SampleData(fullName, sampleName, categoryName);
-                _samples.Add(fullName, sampleData);
-            }
-
-            sampleData.Called(selfMsec, execMsec);
+            return false;
         }
 
         protected override string GetResultText()
         {
             CsvStringGenerator csvStringGenerator = new CsvStringGenerator();
-            csvStringGenerator.AppendColumn("name").AppendColumn("fullname").AppendColumn("category")
-                .AppendColumn("callNum").AppendColumn("self").AppendColumn("sum(msec)").AppendColumn("perFrame(msec)")
-                .AppendColumn("min(msec)").AppendColumn("max(msec)").AppendColumn("total").AppendColumn("sum(msec)")
-                .AppendColumn("perFrame(msec)").AppendColumn("min(msec)").AppendColumn("max(msec)").NextRow();
-            var sampleDataList = new List<SampleData>(_samples.Values);
-            sampleDataList.Sort((a, b) =>
+            csvStringGenerator.AppendColumn("frame");
+
+            foreach (var name in distinctSampleNames)
             {
-                if (a.TotalSelfMsec > b.TotalSelfMsec)
+                csvStringGenerator.AppendColumn(name);
+            }
+
+            csvStringGenerator.NextRow();
+
+            int frameNum = 1;
+            foreach (FrameSampleData sampleData in frameSampleData)
+            {
+                csvStringGenerator.AppendColumn(frameNum);
+                foreach (var sampleName in distinctSampleNames)
                 {
-                    return -1;
+                    sampleData.Samples.TryGetValue(sampleName, out float execMsec);
+                    csvStringGenerator.AppendColumn(execMsec);
                 }
 
-                if (a.TotalSelfMsec < b.TotalSelfMsec)
-                {
-                    return 1;
-                }
-
-                return 0;
-            });
-            foreach (var sampleData in sampleDataList)
-            {
-                csvStringGenerator.AppendColumn(sampleData.sampleName).AppendColumn(sampleData.fullName)
-                    .AppendColumn(sampleData.categoryName).AppendColumn(sampleData.CallNum).AppendColumn("");
-
-                csvStringGenerator.AppendColumn(sampleData.TotalSelfMsec)
-                    .AppendColumn(sampleData.TotalSelfMsec / _frameNum).AppendColumn(sampleData.SelfMinMSec)
-                    .AppendColumn(sampleData.SelfMaxMsec).AppendColumn("");
-
-
-                csvStringGenerator.AppendColumn(sampleData.TotalExecMsec)
-                    .AppendColumn(sampleData.TotalExecMsec / _frameNum).AppendColumn(sampleData.ExecMinMSec)
-                    .AppendColumn(sampleData.ExecMaxMsec);
+                frameNum++;
                 csvStringGenerator.NextRow();
             }
 
@@ -160,7 +94,7 @@ namespace UTJ.ProfilerReader.Analyzer
 
         protected override string FooterName
         {
-            get { return "_main_self.csv"; }
+            get { return "_ai_script.csv"; }
         }
     }
 }
