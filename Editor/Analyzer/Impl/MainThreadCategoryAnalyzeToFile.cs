decfd602887dd75c999b12dc7290b889c34d8f55
Enhancer <xjhxyshz@126.com>
2021-08-06 11:04:39 +0800

    Refactor

--- Editor/Analyzer/Impl/MainThreadCategoryAnalyzeToFile.cs
+++ Editor/Analyzer/Impl/MainThreadCategoryAnalyzeToFile.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UTJ.ProfilerReader.BinaryData;
-
 
 namespace UTJ.ProfilerReader.Analyzer
 {
@@ -12,23 +10,25 @@ namespace UTJ.ProfilerReader.Analyzer
             public Dictionary<string, float> frameData;
             public int frameIdx;
 
-            public void AddData(string category,float msec)
+            public void AddData(string category, float msec)
             {
-                if(frameData == null)
+                if (frameData == null)
                 {
                     frameData = new Dictionary<string, float>();
                 }
+
                 float val;
-                if( frameData.TryGetValue(category,out val) ){
+                if (frameData.TryGetValue(category, out val))
+                {
                     frameData[category] = val + msec;
                 }
                 else
                 {
-                    frameData.Add( category , msec );
+                    frameData.Add(category, msec);
                 }
             }
 
-            public void AppendCsv(CsvStringGenerator csvStringGenerator, List<string> categoriesStr)
+            public void AppendCsv(CsvStringGenerator csvStringGenerator, ICollection<string> categoriesStr)
             {
                 foreach (var category in categoriesStr)
                 {
@@ -38,34 +38,20 @@ namespace UTJ.ProfilerReader.Analyzer
                     {
                         val = 0.0f;
                     }
+
                     csvStringGenerator.AppendColumn(val);
                 }
             }
         }
 
         private string[] categories;
-        private Dictionary<string, int> categoryDictionary;
-        private List<string> categoriesStr;
+        private SortedDictionary<string, int> categoryDictionary;
         private List<FrameByCategory> frames = new List<FrameByCategory>();
 
-        private void CollectThread(ThreadData thread,FrameByCategory frameByCategory)
-        {
-            if (thread.m_AllSamples == null) { return; }
-            foreach (var sample in thread.m_AllSamples)
-            {
-                if( 0 <= sample.group && sample.group < categories.Length )
-                frameByCategory.AddData(categories[sample.group], sample.selfTimeUs * 0.001f);
-            }
-        }
         public override void CollectData(ProfilerFrameData frameData)
         {
-            // Categoryのセットアップ
             SetupCategories();
-            FrameByCategory frameByCategory = new FrameByCategory();
-            frameByCategory.frameIdx = frameData.frameIndex;
-            // 特別枠で frameDataのＣＰＵ時間を追加
-            // 同一フレーム内に同じスレッド名が複数できるので…
-            Dictionary<string, int> threadNameCounter = new Dictionary<string, int>(8);
+            FrameByCategory frameByCategory = new FrameByCategory {frameIdx = frameData.frameIndex};
             foreach (var thread in frameData.m_ThreadData)
             {
                 if (thread.IsMainThread)
@@ -73,28 +59,44 @@ namespace UTJ.ProfilerReader.Analyzer
                     CollectThread(thread, frameByCategory);
                 }
             }
-            this.frames.Add(frameByCategory);
+
+            frames.Add(frameByCategory);
         }
+
         private void SetupCategories()
         {
-            if(this.categoryDictionary != null)
+            if (categoryDictionary != null)
             {
                 return;
             }
-            this.categoriesStr = new List<string>();
-            this.categoryDictionary = new Dictionary<string, int>();
-            categories = UTJ.ProfilerReader.ProtocolData.GetCategories(this.unityVersion);
+
+            categoryDictionary = new SortedDictionary<string, int>();
+            categories = ProtocolData.GetCategories(unityVersion);
+
             int idx = 0;
-            foreach( var item in categories)
+            foreach (var item in categories)
             {
                 if (!categoryDictionary.ContainsKey(item))
                 {
-                    categoriesStr.Add(item);
-                    categoryDictionary.Add(item, idx);
-                    ++idx;
+                    categoryDictionary.Add(item, idx++);
                 }
             }
         }
+
+        private void CollectThread(ThreadData thread, FrameByCategory frameByCategory)
+        {
+            if (thread.m_AllSamples == null)
+            {
+                return;
+            }
+
+            foreach (var sample in thread.m_AllSamples)
+            {
+                if (0 <= sample.group && sample.group < categories.Length)
+                    frameByCategory.AddData(categories[sample.group], sample.selfTimeUs * 0.001f);
+            }
+        }
+
         /// <summary>
         /// 結果書き出し
         /// </summary>
@@ -102,30 +104,26 @@ namespace UTJ.ProfilerReader.Analyzer
         {
             CsvStringGenerator csvStringGenerator = new CsvStringGenerator();
             csvStringGenerator.AppendColumn("frameIdx");
-            foreach( var str in categoriesStr)
+            foreach (var str in categoryDictionary.Keys)
             {
-                csvStringGenerator.AppendColumn(str+"(msec)");
-            }            
+                csvStringGenerator.AppendColumn(str + "(msec)");
+            }
+
             csvStringGenerator.NextRow();
 
-            foreach( var frame in frames)
+            foreach (var frame in frames)
             {
                 csvStringGenerator.AppendColumn(frame.frameIdx);
-                frame.AppendCsv(csvStringGenerator,this.categoriesStr);
+                frame.AppendCsv(csvStringGenerator, categoryDictionary.Keys);
                 csvStringGenerator.NextRow();
             }
 
             return csvStringGenerator.ToString();
         }
-        
+
         protected override string FooterName
         {
-            get
-            {
-                return "_main_thread_category_per_frame.csv";
-            }
+            get { return "_main_thread_category_per_frame.csv"; }
         }
-
     }
-
 }
